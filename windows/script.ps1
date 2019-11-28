9b37a60e06e24dad5187ff5ac7f6d297a2930380
Samuel Vandamme <samuel@sava.be>
2019-11-28 09:32:49 +0100

    Initial commit

---
+++ windows/script.ps1
@@ -0,0 +1,24 @@
+# Run with
+
+param (
+    [string]$newrelicLicenseKey
+)
+
+# Download
+if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem)
+{
+    # 32bit
+    $downloadUrl = "https://download.newrelic.com/infrastructure_agent/windows/386/newrelic-infra-386.msi"
+} else {
+    # 64bit
+    $downloadUrl = "https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.msi"
+}
+Invoke-WebRequest -UseBasicParsing -uri $downloadUrl -outfile "newrelic-infra.msi"
+
+# Install
+msiexec.exe /qn /i ./newrelic-infra.msi GENERATE_CONFIG=true LICENSE_KEY=$newrelicLicenseKey
+
+# Maintain? Can we update automatically? Do we want to
+
+# Start service
+start-service -name newrelic-infra
